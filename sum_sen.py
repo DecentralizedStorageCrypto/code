804e4ed8cf793f3deb14d0107a22dcecb1409584
mansourdavoudi <m.davoudi.official@gmail.com>
2023-01-12 18:48:14 +0330

    new changes added

--- sum_sen.py
+++ sum_sen.py
@@ -18,21 +18,22 @@ mng = mongodb(localhost, db_name)
 
 df = mng.returnColAsDf(collection_name)
 
-for counter in range(df.shape[0]):
+for counter in range(23,24):
 
     title = str(df.iloc[counter]['title'])
-    text = str(df.iloc[counter]['title'])
+    text = str(df.iloc[counter]['text'])
     print(title)
-    print(text)
+
     preprocess_text = text.strip().replace("\n", "")
+    maxLength = len(preprocess_text.split(" ")) / 2
+    print(preprocess_text)
     t5_prepared_Text = "summarize: " + preprocess_text
     tokenized_text = tokenizer.encode(t5_prepared_Text, return_tensors="pt", max_length=2048, truncation=True).to(
         device)
     summary_ids = model.generate(tokenized_text,
-                                 num_beams=4,
-                                 min_length=50,
-                                 max_length=100,
-                                 length_penalty=2.0,
+                                 num_beams=8,
+                                 min_length=100,
+                                 max_length=maxLength
                                  )
     #print(summary_ids, end="\n")
 
@@ -45,18 +46,19 @@ for counter in range(df.shape[0]):
     input2 = tokenizer(summary, return_tensors="pt", padding=True)
     sent_scores_title = finbert(**input1)[0]
     sent_scores_text = finbert(**input2)[0]
-
     print(sent_scores_title)
     print(sent_scores_text)
-
     nmpy_title = sent_scores_title.detach().numpy()
-    nmpy_text = sent_scores_title.detach().numpy()
+    nmpy_text = sent_scores_text.detach().numpy()
     x1 = nmpy_title[0]
     x2 = nmpy_text[0]
     sft_title = (np.exp(x1) / np.exp(x1).sum())
     sft_text = (np.exp(x2) / np.exp(x2).sum())
-    final_sent = np.concatenate((sft_title, sft_text), axis=0)
-    print(final_sent)
-    break
-    # labels = {0: 'neutral', 1: 'positive', 2: 'negative'}
-    # print(labels[np.argmax(sent_scores.detach().numpy())])
+    med_snt = np.vstack((sft_title, sft_text))
+    print(med_snt)
+    final_snt = np.mean(med_snt, axis=0)
+    print(final_snt)
+    labels = {0: 'neutral', 1: 'positive', 2: 'negative'}
+    print(labels[np.argmax(sft_title)])
+    print(labels[np.argmax(sft_text)])
+    print(labels[np.argmax(final_snt)])
