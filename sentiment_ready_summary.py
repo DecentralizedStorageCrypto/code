a36a92a3466fdd62ba9f02903fcd9adf31807a90
mansourdavoudi <m.davoudi.official@gmail.com>
2023-01-17 10:16:26 +0330

    new changes added

---
+++ sentiment_ready_summary.py
@@ -0,0 +1,72 @@
+import torch
+import json
+from transformers import T5Tokenizer, T5ForConditionalGeneration,T5Config
+from transformers import BertTokenizer, BertForSequenceClassification
+import numpy as np
+import torch.nn as tornn
+from db import mongodb
+
+
+model = T5ForConditionalGeneration.from_pretrained('t5-large')
+tokenizer = T5Tokenizer.from_pretrained('t5-large')
+device = torch.device('cpu')
+localhost = "mongodb://127.0.0.1:27017"
+db_name = "news"
+collection_name = "filecoinNews"
+
+mng = mongodb(localhost, db_name)
+
+df = mng.returnColAsDf(collection_name)
+
+for counter in range(df.shape[0]):
+    title = str(df.iloc[counter]['title'])
+    text = str(df.iloc[counter]['text'])
+    key_phrases = df.iloc[counter]['KeyPhrase']
+    print(title)
+    preprocess_text = text.strip().replace("\n", "")
+    maxLength = int(len(preprocess_text.split(" ")) / 2)
+    minLength = int(len(preprocess_text.split(" ")) / 5)
+    print(preprocess_text)
+    summary = str(df.iloc[counter]['summary'])
+    print("\n\n summary: ", summary, end="\n")
+
+    finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone', num_labels=3)
+    tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
+
+    input1 = tokenizer(title, return_tensors="pt", padding=True)
+    input2 = tokenizer(summary, return_tensors="pt", padding=True)
+    sent_scores_title = finbert(**input1)[0]
+    sent_scores_text = finbert(**input2)[0]
+    # print(sent_scores_title)
+    # print(sent_scores_text)
+    phrase_score_list = []
+    labels = {0: 'neutral', 1: 'positive', 2: 'negative'}
+    for item in key_phrases:
+        phrase = item['phrase']
+        input = tokenizer(phrase, return_tensors="pt", padding=True)
+        sent_scores_phrase = finbert(**input)[0]
+        nmpy_phrase = sent_scores_phrase.detach().numpy()
+        x = nmpy_phrase[0]
+        sft_phrase = (np.exp(x) / np.exp(x).sum())
+        print(phrase, "-->", sent_scores_phrase, labels[np.argmax(sft_phrase)], end="\n")
+        phrase_score_list.append(sft_phrase)
+    #print(phrase_score_list)
+    phrase_nmp = np.array(phrase_score_list)
+    final_snt_phrase = np.mean(phrase_nmp, axis=0)
+    print("\n The average sentiment score of key phrases is: ", final_snt_phrase, end="\n\n")
+    nmpy_title = sent_scores_title.detach().numpy()
+    nmpy_text = sent_scores_text.detach().numpy()
+    x1 = nmpy_title[0]
+    x2 = nmpy_text[0]
+    sft_title = (np.exp(x1) / np.exp(x1).sum())
+    sft_text = (np.exp(x2) / np.exp(x2).sum())
+    print("The sentiment score of title is: ", sft_title, end="\n\n")
+    print("The sentiment score of summary is: ", sft_text, end="\n\n")
+    med_snt = np.vstack((sft_title, sft_text, final_snt_phrase))
+    print(med_snt, end="\n\n\n")
+    final_snt = np.mean(med_snt, axis=0)
+    print("The average sentiment score of title, key phrases and summary is: ", final_snt, end="\n\n")
+    print("The average sentiment of key phrases is:", labels[np.argmax(final_snt_phrase)], end="\n\n")
+    print("The sentiment of title is:", labels[np.argmax(sft_title)], end="\n\n")
+    print("The sentiment of summary is:", labels[np.argmax(sft_text)], end="\n\n")
+    print("The average sentiment of title, key phrases, and summary, is:", labels[np.argmax(final_snt)], end="\n\n")
