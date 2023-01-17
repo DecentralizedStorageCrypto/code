118b023b1920c0c23dd8c08677bc17d0c98b129e
mansourdavoudi <m.davoudi.official@gmail.com>
2023-01-17 17:29:39 +0330

    new changes added

--- networkConsole.py
+++ networkConsole.py
@@ -1,10 +1,13 @@
 from manageNetwork import network
 
 
-net = network('bolt://localhost:7687', 'neo4j', '228218')
+net = network('bolt://localhost:7687', 'neo4j', 'burna-36')
 
-net.add_node_category("categories", "network")
-net.add_edge_category("categories", "network")
-net.add_node_entity("entities", "network")
-net.add_edge_entity("entities", "network")
-net.add_edge_relations("entities", "relations", "network")
+db_name = "neo4j"
+
+net.removeNet(db_name)
+net.add_node_category("categories", db_name)
+net.add_edge_category("categories", db_name)
+net.add_node_entity("entities", db_name)
+net.add_edge_entity("entities", db_name)
+net.add_edge_relations("entities", "relations", db_name)
