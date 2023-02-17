f53d5f7768de900ba9b24b2ab75e6103c0e3081e
mansourdavoudi <m.davoudi.official@gmail.com>
2023-02-18 01:05:03 +0330

    classifier added

--- addTweetSentiment.py
+++ addTweetSentiment.py
@@ -16,25 +16,22 @@ def addSnt(start, end):
 
     for counter in range(start, end):
         try:
-            chk = str(df.iloc[counter]['aggScore'])
-            if chk == "nan":
-                print(counter)
-                _id = df['_id'][counter]
-                tokenizedText = str(df.iloc[counter]['tokenizedTweet'])
-                lst = (list(eval(tokenizedText)))
-                tweetText = ' '.join(lst)
-                finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone', num_labels=3)
-                tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
-                input1 = tokenizer(tweetText, return_tensors="pt", padding=True)
-                sent_scores_tweet = finbert(**input1)[0]
-                labels = {0: 'neutral', 1: 'positive', 2: 'negative'}
-                nmpy_tweet = sent_scores_tweet.detach().numpy()
-                x1 = nmpy_tweet[0]
-                sft_tweet = (np.exp(x1) / np.exp(x1).sum())
-                print("The sentiment score of tweet is:", sft_tweet, end="\n")
-                print("The sentiment of tweet is:", labels[np.argmax(sft_tweet)], end="\n\n")
-                mng.addAggSentimentScore(collection_name, _id, str(sft_tweet))
-                mng.addAggSentimentLabel(collection_name, _id, str(labels[np.argmax(sft_tweet)]))
+            _id = df['_id'][counter]
+            tokenizedText = str(df.iloc[counter]['tokenizedTweet'])
+            lst = (list(eval(tokenizedText)))
+            tweetText = ' '.join(lst)
+            finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone', num_labels=3)
+            tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
+            input1 = tokenizer(tweetText, return_tensors="pt", padding=True)
+            sent_scores_tweet = finbert(**input1)[0]
+            labels = {0: 'neutral', 1: 'positive', 2: 'negative'}
+            nmpy_tweet = sent_scores_tweet.detach().numpy()
+            x1 = nmpy_tweet[0]
+            sft_tweet = (np.exp(x1) / np.exp(x1).sum())
+            print("The sentiment score of tweet is:", sft_tweet, end="\n")
+            print("The sentiment of tweet is:", labels[np.argmax(sft_tweet)], end="\n\n")
+            mng.addAggSentimentScore(collection_name, _id, str(sft_tweet))
+            mng.addAggSentimentLabel(collection_name, _id, str(labels[np.argmax(sft_tweet)]))
 
         except Exception as e:
             print(str(e))
@@ -47,9 +44,9 @@ if __name__ == "__main__":
 #batch size determines, number of sentences that assigns to each thread.
     batch_size = 1000
 #total size, determines total number of news articles for summary etraction and sentiment analysis.
-    total = 30000
+    total = 120000
 #base determines the starting point of news articles.
-    base = 60000
+    base = 90000
     counter = int(total/batch_size)
     threads = []
     for i in range(counter):
